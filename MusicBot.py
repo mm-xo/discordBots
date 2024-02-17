9768d8c1cd0fd26a5c4cf253b7096580b7b919b2
mm-xo <manmilan@myumanitoba.ca>
2024-02-17 11:10:29 -0600

    add join/leave commands

--- MusicBot.py
+++ MusicBot.py
@@ -8,7 +8,10 @@ async def join(ctx):
 	channel = ctx.author.voice.channel
 	await channel.connect()
 
-
+@bot.command()
+async def leave(ctx):
+	# channel = ctx.author.voice.channel
+	await ctx.voice_client.disconnect()
 
 
 
